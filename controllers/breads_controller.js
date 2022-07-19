dd8aabcfb290fb78b91b9f928a023866144604e0
Zachary Davis <kicksilver@icloud.com>
2022-07-19 19:09:48 -0400

    added bread

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const breads = express.Router()
+const Bread = require('../models/bread.js')
 
 //index
 breads.get('/', (req, res) => {
-    res.send('Index for /breads')
+    res.send(Bread)
 })
+
+breads.get('/:arrayIndex', (req, res) => {
+res.send(Bread[req.params.arrayIndex])
+})
+
 
 module.exports = breads
