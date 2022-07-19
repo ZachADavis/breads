fe8f50b97e11c357a2354138fcc6bf33951a91b0
Zachary Davis <kicksilver@icloud.com>
2022-07-19 18:48:33 -0400

    Added server and packages

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 
+//Required modules
+const express = require('express')
+
+//config
+require('dotenv').config()
+const PORT = process.env.PORT
+
+//initializing app
+const app = express()
+
+//routes
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads!')
+})
+
+//listen
+app.listen(PORT, () => {
+    console.log(PORT);
+})
